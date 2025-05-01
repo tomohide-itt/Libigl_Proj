373dbaa55fb2928d4744500cd11e693bbf54f138
tomohide-itt <gtakeyamatomohide@gmail.com>
2025-05-01 17:15:10 +0900

    T002

---
+++ T002_geo/main.cpp
@@ -0,0 +1,15 @@
+#include <igl/cotmatrix.h>
+#include <Eigen/Dense>
+#include <iostream>
+#include "libs/gmsh/gmsh.h"
+
+int main(int argc, char *argv[])
+{
+  gmsh::geo geo;
+  // Read the geo file
+  geo.read(DATA_PATH "/ground_model.geo");
+
+  
+
+  return 0;
+}
